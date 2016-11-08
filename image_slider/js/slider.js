f644e8756966aec37a93cb11c8ad048c8d0e6440
SecretKosmoNaut <alexander.sosa1993@gmail.com>
2016-11-08 12:45:50 -0800

    Add files via upload

--- image_slider/js/slider.js
+++ image_slider/js/slider.js
@@ -1,71 +1,54 @@
-sliderInt = 1;
-sliderNext = 2;
-count = $("#slider img").length;
+slideInt = 1;
+slideNext = 2;
+var count = $("#slider img").length;
 
 $(document).ready(function() {
-
-	$("#slider img#" + sliderInt).fadeIn(300);
-
-	startSlider();
+	$("#slider img#" + slideInt).fadeIn(300);
+	startSlider();	
 });
 
 function startSlider() {
-
 	loop = setInterval(function() {
-
-		if(sliderNext > count) {
-			sliderNext = 1;
+		if(slideNext > count) {
+			slideNext = 1;		
 		};
-
 		$("#slider img").fadeOut(300);
-		$("#slider img#" + sliderNext).fadeIn(300);
-		sliderInt = sliderNext;
-		sliderNext = sliderNext + 1;
-
+		$("#slider img#" + slideNext).fadeIn(300);
+		slideInt = slideNext;
+		slideNext++;
 	}, 3000);
-
 };
 
 function stopSlider() {
-
 	window.clearInterval(loop);
-
 };
 
-$("#slider img").hover(function() {stopSlider();}, function(){startSlider();});
+$("img").hover(function() {
+	stopSlider();
+}, function() {
+	startSlider();
+});
+
+function showSlide(slide) {
+	stopSlider();
+	if(slide > count) {
+		slide = 1;
+	} else if(slide < 1) {
+		slide = count;
+	};
+	$("#slider img").fadeOut(300);
+	$("#slider img#" + slide).fadeIn(300);
+	slideInt = slide;
+	slideNext = slide + 1;
+	startSlider();
+};
 
 function prev() {
-
-	newSlide = sliderInt - 1;
+	newSlide = slideInt - 1;
 	showSlide(newSlide);
-
 };
 
 function next() {
-
-	newSlide = sliderInt + 1;
+	newSlide = slideInt + 1;
 	showSlide(newSlide);
-
-};
-
-function showSlide(id) {
-
-	stopSlider();
-	
-	if(id > count) {
-
-			id = 1;
-
-	}else if(id < 1) {
-
-		id = count;
-
-	};
-
-		$("#slider img").fadeOut(300);
-		$("#slider img#" + id).fadeIn(300);
-		sliderInt = id;
-		sliderNext = id + 1;
-
-	startSlider();
 };
